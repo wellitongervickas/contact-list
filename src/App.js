1e65a45979a777da8f281a9baec8e68ce77567de
Welliton Gervickas <wellitogervickas@gmail.com>
2018-04-26 21:08:13 -0300

    Create a style of pages and change read-me of project details

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import routes from './models/system/routes-system';
 
 const App = () => (
- <Router>
-    <div style={{ display: "flex" }}>
-      <div>
-        <ul style={{ listStyleType: "none", padding: 0 }}>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/messages">messages</Link>
-          </li>
-          <li>
-            <Link to="/contacts">contacts</Link>
-          </li>
-        </ul>
-      </div>
-      <div>
-        {routes.map((route, index) => (
-          <Route
-            key={index}
-            path={route.path}
-            exact={route.exact}
-            component={route.main}
-          />
-        ))}
-      </div>
+  <Router>
+    <div>
+      {routes.map((route, index) => (
+        <Route
+          key={index}
+          path={route.path}
+          exact={route.exact}
+          component={route.main}
+        />
+      ))}
     </div>
   </Router>
 );
